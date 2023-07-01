3120afbf59dde77679a277e613a838b844452042
imbizman316 <pride81_@hotmail.com>
2023-07-01 14:17:13 +0900

    Change rock papaer scissor

--- recipes/scripts/rockpapperscissor.js
+++ recipes/scripts/rockpapperscissor.js
@@ -8,6 +8,17 @@ const scissorbutton = document.querySelector("#scissor");
 
 //Get the bottom so we can change it, telling what computer drew.
 const result = document.querySelector(".result");
+const playerScoreDis = document.querySelector(".playerScore");
+const enemyScoreDis = document.querySelector(".enemyScore");
+const grandTotal = document.querySelector(".grandtotal");
+
+let roundCount = 0;
+
+let playerScore = 0;
+let enemyScore = 0;
+playerScoreDis.innerText = `Player: ${playerScore}`;
+enemyScoreDis.innerText = `Enemy: ${enemyScore}`;
+
 
 //Get the bottom button
 const playAgain = document.querySelector(".playagain");
@@ -34,24 +45,45 @@ function generate(event){
     //show their plays on the bottom.
     result.innerHTML = `<h3>Player: ${player} VS. Enemy: ${randNum}</h3>`
 
-
     //Compare it with player's input with if statement.
-    
-
     if (player === 'rock' && randNum === 'scissor') {
         showresult("player");
+        playerScore ++;
     }
     else if (player === 'scissor' && randNum === 'paper') {
         showresult("player");
+        playerScore ++;
     }
     else if (player === 'paper' && randNum === 'rock') {
         showresult("player");
+        playerScore ++;
     }
     else if (player === randNum) {
         showresult("draw");
+
     } else {
         showresult("enemy");
+        enemyScore ++;
     }
+    playerScoreDis.innerText = `Player: ${playerScore}`;
+    enemyScoreDis.innerText = `Enemy: ${enemyScore}`;
+    roundCount ++;
+    
+    if (enemyScore === 5) {
+        grandTotal.innerText = "Enemy is the final winner";
+        playAgain.style.display = "block";
+        rockbutton.disabled = true;
+        paperbutton.disabled = true;
+        scissorbutton.disabled = true;
+    }
+    else if (playerScore === 5) {
+        grandTotal.innerText = "Player is the final winner";
+        playAgain.style.display = "block";
+        rockbutton.disabled = true;
+        paperbutton.disabled = true;
+        scissorbutton.disabled = true;
+    }
+
 }
 
 function showresult(outcome){
@@ -59,7 +91,6 @@ function showresult(outcome){
     if (outcome === "player") {header.innerHTML = "<h1>You win!</h1>";}
     else if (outcome === "enemy") {header.innerHTML = "<h1>AI wins!</h1>";}
     else {header.innerHTML = "<h1>It's a DRAW</h1>";}
-    playAgain.style.display = "block";
 }
 
 function replay() {
@@ -68,6 +99,15 @@ function replay() {
     playAgain.style.display = "none";
     header.innerHTML = "<h1>Make a choice</h1>";
     result.innerHTML = "<h3></h3>"
+    playerScore = 0;
+    enemyScore = 0;
+    playerScoreDis.innerText = `Player: ${playerScore}`;
+    enemyScoreDis.innerText = `Enemy: ${enemyScore}`;
+    grandTotal.innerText = "";
+    rockbutton.disabled = false;
+    paperbutton.disabled = false;
+    scissorbutton.disabled = false;
+
 }
 
 
